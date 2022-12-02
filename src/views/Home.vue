a2a9ceb41fc795a6a77c1a95364415ebbf9bd23e
Maciej Klis <klismaciej.97@gmail.com>
2022-12-02 13:31:50 +0100

    Testout composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,30 @@
-<script lang="ts"></script>
+<script lang="ts">
+import { ref } from 'vue';
+export default {
+    name: "Home",
+    setup() {
+        let someVariable = ref('Siemanko');
+        const clikedTimes = ref(0);
+        const h1 = ref(null);
+
+        const logH1 = () => {
+            someVariable.value = "siemaa"
+        }
+
+        return {
+            someVariable,
+            clikedTimes,
+            h1,
+            logH1
+        }
+    },
+}
+</script>
 
 <template>
     <div>
-        Home Component
+        <h1 ref="h1"> Component </h1>
+        <button @click="(someVariable += ' ' + (clikedTimes++))">Click</button>
+        <button @click="logH1">Log h1</button>   
     </div>
 </template>
